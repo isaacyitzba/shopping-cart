b3e197785a4d3a54d3ac55792d89f72c47e84810
Isaac BA <iba.burton@gmail.com>
2016-11-16 17:03:54 +0200

    woooo

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
 // an array with all of our cart items
 var cart = [];
+var total = 0;
 
 var updateCart = function () {
-  // TODO: finish
-}
+  // TODO: finis
+  // var source = $('#cart-template').html();
+  // var template = Handlebars.compile(source);
+  $('.total').empty();
+  total=0;
+  for(var  i = 0; i < cart.length; i++){
+    total += cart[i].price;
+    } 
+  $('.total').append(total);
 
+  
+  $('.cart-list').empty();
+
+  var source = $('#cart-template').html();
+  var template = Handlebars.compile(source);
+
+  for(var i = 0; i < cart.length; i++){
+
+    var newHTML = template(cart[i]);
+    $('.cart-list').append(newHTML);
+  }
+}
 
 var addItem = function (item) {
   // TODO: finish
+  cart.push(item);
 }
 
 var clearCart = function () {
   // TODO: finish
+    $('.cart-list').empty();
+    $('.total').empty();
+  
 }
 
 $('.view-cart').on('click', function () {
   // TODO: hide/show the shopping cart!
+  $('.shopping-cart').toggle();
 });
 
 $('.add-to-cart').on('click', function () {
   // TODO: get the "item" object from the page
+  var item = {
+    name:$(this).closest('.item').data().name,
+    price:$(this).closest('.item').data().price
+  }
   addItem(item);
   updateCart();
 });
